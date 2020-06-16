8f050bb5cda526d02224c80da0908a960931c0f1
ylean <258258001@qq.com>
2020-06-16 17:16:53 +0800

    put 参数

--- expand/src/main/java/com/ylean/expand/network/builder/PutBuilder.java
+++ expand/src/main/java/com/ylean/expand/network/builder/PutBuilder.java
@@ -5,6 +5,9 @@ import com.ylean.expand.network.callback.MyCallback;
 import com.ylean.expand.network.response.IResponseHandler;
 import com.ylean.expand.utils.Log;
 
+import java.util.Map;
+
+import okhttp3.FormBody;
 import okhttp3.MediaType;
 import okhttp3.Request;
 import okhttp3.RequestBody;
@@ -13,12 +16,21 @@ import okhttp3.RequestBody;
  * put builder
  * Created by tsy on 16/12/06.
  */
-public class PutBuilder extends OkHttpRequestBuilder<PutBuilder> {
+public class PutBuilder extends OkHttpRequestBuilderHasParam<PutBuilder> {
 
+    private String mJsonParams = "";
     public PutBuilder(MyOkHttp myOkHttp){
         super(myOkHttp);
     }
-
+    /**
+     * json格式参数
+     * @param json
+     * @return
+     */
+    public PutBuilder jsonParams(String json) {
+        this.mJsonParams = json;
+        return this;
+    }
     @Override
     public void enqueue(IResponseHandler responseHandler) {
         try {
@@ -33,16 +45,29 @@ public class PutBuilder extends OkHttpRequestBuilder<PutBuilder> {
                 builder.tag(mTag);
             }
 
-            builder.put(RequestBody.create(MediaType.parse("text/plain;charset=utf-8"), ""));
-
+            if(mJsonParams.length() > 0) {      //上传json格式参数
+                RequestBody body = RequestBody.create(MediaType.parse("application/json; charset=utf-8"), mJsonParams);
+                builder.put(body);
+            } else {        //普通kv参数
+                FormBody.Builder encodingBuilder = new FormBody.Builder();
+                appendParams(encodingBuilder, mParams);
+                builder.put(encodingBuilder.build());
+            }
             Request request = builder.build();
-
             mMyOkHttp.getOkHttpClient()
                     .newCall(request)
                     .enqueue(new MyCallback(responseHandler));
         } catch (Exception e) {
             Log.e("Put enqueue error:" + e.getMessage());
             responseHandler.onFailure(0, e.getMessage());
+        }
+    }
+    private void appendParams(FormBody.Builder builder, Map<String, String> params) {
+
+        if (params != null && !params.isEmpty()) {
+            for (String key : params.keySet()) {
+                builder.add(key, params.get(key));
+            }
         }
     }
 }
