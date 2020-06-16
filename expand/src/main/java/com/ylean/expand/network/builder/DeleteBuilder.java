b4eee993aef4b82821cf1f2e30dcf00d146a3e83
ylean <258258001@qq.com>
2020-06-16 17:51:53 +0800

    网络请求 delete 新增参数支持

--- expand/src/main/java/com/ylean/expand/network/builder/DeleteBuilder.java
+++ expand/src/main/java/com/ylean/expand/network/builder/DeleteBuilder.java
@@ -5,19 +5,32 @@ import com.ylean.expand.network.callback.MyCallback;
 import com.ylean.expand.network.response.IResponseHandler;
 import com.ylean.expand.utils.Log;
 
+import java.util.Map;
+
+import okhttp3.FormBody;
+import okhttp3.MediaType;
 import okhttp3.Request;
+import okhttp3.RequestBody;
 
 /**
  * delete builder
  * Created by tsy on 2016/12/6.
  */
 
-public class DeleteBuilder extends OkHttpRequestBuilder<DeleteBuilder> {
-
+public class DeleteBuilder extends OkHttpRequestBuilderHasParam<DeleteBuilder> {
+    private String mJsonParams = "";
     public DeleteBuilder(MyOkHttp myOkHttp) {
         super(myOkHttp);
     }
-
+    /**
+     * json格式参数
+     * @param json
+     * @return
+     */
+    public DeleteBuilder jsonParams(String json) {
+        this.mJsonParams = json;
+        return this;
+    }
     @Override
     public void enqueue(final IResponseHandler responseHandler) {
         try {
@@ -31,7 +44,14 @@ public class DeleteBuilder extends OkHttpRequestBuilder<DeleteBuilder> {
             if (mTag != null) {
                 builder.tag(mTag);
             }
-
+            if(mJsonParams.length() > 0) {      //上传json格式参数
+                RequestBody body = RequestBody.create(MediaType.parse("application/json; charset=utf-8"), mJsonParams);
+                builder.delete(body);
+            } else {        //普通kv参数
+                FormBody.Builder encodingBuilder = new FormBody.Builder();
+                appendParams(encodingBuilder, mParams);
+                builder.delete(encodingBuilder.build());
+            }
             Request request = builder.build();
 
             mMyOkHttp.getOkHttpClient()
@@ -40,6 +60,13 @@ public class DeleteBuilder extends OkHttpRequestBuilder<DeleteBuilder> {
         } catch (Exception e) {
             Log.e("Delete enqueue error:" + e.getMessage());
             responseHandler.onFailure(0, e.getMessage());
+        }
+    }private void appendParams(FormBody.Builder builder, Map<String, String> params) {
+
+        if (params != null && !params.isEmpty()) {
+            for (String key : params.keySet()) {
+                builder.add(key, params.get(key));
+            }
         }
     }
 }
